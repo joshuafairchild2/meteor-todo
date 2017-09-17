16cdfa3fe346aa7ec793cd51a7aeddb3a8e9ea6e
Joshua Fairchild <Joshua.fairchild429@gmail.com>
2017-09-16 17:21:11 -0700

    add a checkbox the filters the type of tasks to be displayed using a reactive deictionary, as well as a method that counts all of the incomplete tasks

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,4 +1,5 @@
 import { Template } from 'meteor/templating';
+import { ReactiveDict } from 'meteor/reactive-dict';
 
 import { Tasks } from './../api/tasks.js';
 
@@ -6,9 +7,21 @@ import './task.html';
 import './task.js';
 import './body.html';
 
+Template.body.onCreated(function bodyOnCreated() {
+  this.state = new ReactiveDict();
+});
+
 Template.body.helpers({
   tasks() {
-    return Tasks.find({}, { sort: { createdAt: -1 } });
+    const instance = Template.instance();
+    if (instance.state.get('hideCompleted'))
+      return Tasks.find({ checked: { $ne: true} }, { sort: { createdAt: -1 } });
+    else
+      return Tasks.find({}, { sort: { createdAt: -1 } });
+  },
+
+  incompleteCount() {
+    return Tasks.find({ checked: { $ne: true} }).count();
   }
 });
 
@@ -24,5 +37,9 @@ Template.body.events({
     });
 
     event.target.text.value = '';
+  },
+
+  'change .hide-completed input'(event, instance) {
+    instance.state.set('hideCompleted', event.target.checked);
   }
 });
